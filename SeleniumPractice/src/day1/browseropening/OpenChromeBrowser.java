fcd7ff2a4f7aeff3e88007f78b3946a29276cdc5
MangeshA1988 <mangeshahire.rgis@gmail.com>
2024-03-19 23:36:28 +0530

    My First comment

---
+++ SeleniumPractice/src/day1/browseropening/OpenChromeBrowser.java
@@ -0,0 +1,13 @@
+package day1.browseropening;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenChromeBrowser {
+
+	public static void main(String[] args) {
+		
+	ChromeDriver cdriver=new ChromeDriver();
+
+	}
+
+}
---
+++ SeleniumPractice/src/day1/browseropening/OpenFirefoxBrowser.java
@@ -0,0 +1,13 @@
+package day1.browseropening;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenFirefoxBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FirefoxDriver fdriver=new FirefoxDriver();
+
+	}
+
+}
